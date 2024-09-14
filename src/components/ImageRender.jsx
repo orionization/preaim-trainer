7903732736084a2b395b970a9660a716fc998611
DESKTOP-VB2OALT\Andromeda <origgs@proton.me>
2024-09-14 15:47:41 -0500

    prettier

--- src/components/ImageRender.jsx
+++ src/components/ImageRender.jsx
@@ -1,44 +1,45 @@
 import React from "react";
 import angle from "../images/SAMPLE_angle.jpg";
 import answer from "../images/SAMPLE_answer.jpg";
-import crosshare from "../images/where_u_clicked.png"
+import crosshare from "../images/where_u_clicked.png";
 import Crosshair from "./Crosshair";
-import { useState, useMemo,useCallback } from "react";
+import { useState, useMemo, useCallback } from "react";
 
+export default function ImageRender() {
+  const [crosshairLocation, setCrosshairLocation] = useState(null);
 
+  const image = useMemo(() => {
+    if (crosshairLocation) {
+      return answer;
+    }
+    return angle;
+  }, [crosshairLocation]);
 
-export default function ImageRender (){
-    const [crosshairLocation, setCrosshairLocation] = useState(null);
-
-    const image = useMemo(() => {
-        if (crosshairLocation) {
-            return answer
-        }
-        return angle
-    }, [crosshairLocation])
-
-    const handleClick = useCallback((e) => {
-        e.preventDefault()
-        const {
-            clientX, 
-            clientY, 
+  const handleClick = useCallback(
+    (e) => {
+      e.preventDefault();
+      const {
+        clientX,
+        clientY,
         //    ...rest
-        } = e
-        setCrosshairLocation((prev) => {
-            if (prev) {
-                return null
-            }
-            return {
-                x: clientX,
-                y: clientY
-            }
-        })
-    }, [setCrosshairLocation]) 
+      } = e;
+      setCrosshairLocation((prev) => {
+        if (prev) {
+          return null;
+        }
+        return {
+          x: clientX,
+          y: clientY,
+        };
+      });
+    },
+    [setCrosshairLocation]
+  );
 
-    return (
-        <div className="App">
-            <Crosshair location={crosshairLocation} height={10} image={crosshare}/>
-            <img src={image} onClick={handleClick}/>
-        </div>
-    )
+  return (
+    <div className="App">
+      <Crosshair location={crosshairLocation} height={10} image={crosshare} />
+      <img src={image} onClick={handleClick} />
+    </div>
+  );
 }
